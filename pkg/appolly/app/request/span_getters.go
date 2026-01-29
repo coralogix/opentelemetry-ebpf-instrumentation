f79adfceca8459ebe792a84eb19ae1d50f151e17
Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>
2026-01-29 21:04:02 +0100

    WIP: update rtt metric with new labels (direction, server.port), add iters in the appnetworktracer
    Signed-off-by: Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>

--- pkg/appolly/app/request/span_getters.go
+++ pkg/appolly/app/request/span_getters.go
@@ -67,7 +67,16 @@ func spanOTELGetters(name attr.Name) (attributes.Getter[*Span, attribute.KeyValu
 			return ServerAddr(HostAsServer(s))
 		}
 	case attr.ServerPort:
-		getter = func(s *Span) attribute.KeyValue { return ServerPort(s.HostPort) }
+		getter = func(s *Span) attribute.KeyValue {
+			if s.Type == EventTypeAppNetTcpRtt {
+				if s.AppNet.TcpRtt.Direction == 0 { // TODO (pinoOgni) remove hardcoded value
+					return ServerPort(s.PeerPort)
+				} else {
+					return ServerPort(s.HostPort)
+				}
+			}
+			return ServerPort(s.HostPort)
+		}
 	case attr.RPCMethod:
 		getter = func(s *Span) attribute.KeyValue {
 			if s.Type == EventTypeHTTPClient && s.SubType == HTTPSubtypeAWSS3 && s.AWS != nil {
@@ -318,10 +327,14 @@ func spanOTELGetters(name attr.Name) (attributes.Getter[*Span, attribute.KeyValu
 		}
 	case attr.DNSQuestionName:
 		getter = func(span *Span) attribute.KeyValue { return DNSQuestionName(span.Path) }
-	case attr.AppNetworkTcpRttNetns:
-		getter = func(span *Span) attribute.KeyValue { return AppNetNetns(int(span.AppNet.TcpRtt.Netns)) }
-	case attr.AppNetworkTcpRttPort:
-		getter = func(span *Span) attribute.KeyValue { return AppNetPort(int(span.AppNet.TcpRtt.Port)) }
+	case attr.AppNetworkDirection:
+		getter = func(s *Span) attribute.KeyValue {
+			if s.AppNet.TcpRtt.Direction == 0 { // TODO (pinoOgni) remove hardcoded value
+				return AppNetworkDirection("inbound")
+			} else {
+				return AppNetworkDirection("outbound")
+			}
+		}
 	}
 	// default: unlike the Prometheus getters, we don't check here for service name nor k8s metadata
 	// because they are already attributes of the Resource instead of the attributes.
