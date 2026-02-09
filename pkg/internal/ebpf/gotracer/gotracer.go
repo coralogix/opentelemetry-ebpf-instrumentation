a278c7d0bc9ebd3c442e8833a1423e480bf6fdcd
Mattia Meleleo <mattia.meleleo@coralogix.com>
2026-02-09 16:06:51 +0100

    bpf: gotracer: set grpc server context in shared map

--- pkg/internal/ebpf/gotracer/gotracer.go
+++ pkg/internal/ebpf/gotracer/gotracer.go
@@ -258,8 +258,11 @@ func (p *Tracer) GoProbes() map[string][]*ebpfcommon.ProbeDesc {
 	m := map[string][]*ebpfcommon.ProbeDesc{
 		// Go runtime
 		"runtime.newproc1": {{
-			Start: p.bpfObjects.ObiUprobeProcNewproc1,
-			End:   p.bpfObjects.ObiUprobeProcNewproc1Ret,
+			Start: p.bpfObjects.ObiUprobeRuntimeNewproc1,
+			End:   p.bpfObjects.ObiUprobeRuntimeNewproc1Return,
+		}},
+		"runtime.casgstatus": {{
+			Start: p.bpfObjects.ObiUprobeRuntimeCasgstatus,
 		}},
 		// Go net/http
 		"net/http.serverHandler.ServeHTTP": {{
