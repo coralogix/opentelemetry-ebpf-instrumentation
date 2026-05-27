62476a6fa63d2d859322a198b9f33aad3eaa6856
Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>
2026-05-27 14:16:04 +0200

    Build again main image for demo
    Signed-off-by: Giuseppe Ognibene <giuseppe.ognibene@coralogix.com>

--- bpf/statsolly/maps/stats_events.h
+++ bpf/statsolly/maps/stats_events.h
@@ -10,19 +10,11 @@
 
 struct {
     __uint(type, BPF_MAP_TYPE_RINGBUF);
-    __uint(max_entries, 1 << 20);
+    __uint(max_entries, 1 << 16);
     __uint(pinning, OBI_PIN_INTERNAL);
 } stats_events SEC(".maps");
 
-static volatile u64 __stats_submission_count = 0;
-
 static __always_inline long stats_events_flags() {
     const u64 avail_data = bpf_ringbuf_query(&stats_events, BPF_RB_AVAIL_DATA);
-    const u64 n = __stats_submission_count;
-    __sync_fetch_and_add(&__stats_submission_count, 1);
-    if (n % 1000 == 0) {
-        bpf_printk("stats_events: submissions=%llu avail_data=%llu", n, avail_data);
-    }
     return avail_data >= 4096 ? BPF_RB_FORCE_WAKEUP : BPF_RB_NO_WAKEUP;
 }
-
